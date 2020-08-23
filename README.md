7a9e178f6057c2f44b147348497b999242fec285
mirai-white <1163302785@qq.com>
2020-08-24 01:00:36 +0800

    更新 README.md 初始化 Vue

---
+++ README.md
@@ -0,0 +1,13 @@
+# mini-vue
+
+实现一个简单的 Vue.js。用于理解 Vue响应式原理，妈妈再也不用担心我不会用 Vue了！
+
+## 完整版Vue响应式原理
+
+> 图片引自 [孟思行 - 图解 Vue 响应式原理](https://juejin.im/post/6857669921166491662)
+
+![完整版Vue响应式原理](https://s1.ax1x.com/2020/08/23/dBGXPH.png)
+
+## 乞丐版Vue
+
+![乞丐版Vue](https://s1.ax1x.com/2020/08/24/dBUxD1.jpg)
---
+++ core/vue.js
@@ -0,0 +1,27 @@
+class Vue {
+  constructor(options) {
+    // 1. 保持 options的数据
+    this.$options = options || {}
+    this.$data = options.data || {}
+    this.$el = typeof options.el === 'string' ? document.querySelector(options.el) : options.el
+    // 2. 把 data中的成员转换成 getter和 setter，并注入到 Vue实例中
+    this._proxyData(this.$data)
+  }
+  _proxyData(data) {
+    Object.keys(data).forEach(key => {
+      Object.defineProperty(this, key, {
+        enumerable: true,
+        configurable: true,
+        get() {
+          return data[key]
+        },
+        set(newValue) {
+          if (newValue === data[key]) {
+            return
+          }
+          data[key] = newValue
+        }
+      })
+    })
+  }
+}
